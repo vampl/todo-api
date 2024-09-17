6e5a1f0836f761be34ebc782f08315a87b106f3a
Vitalii Barabash <vitalii_barabash@epam.com>
2024-09-17 19:59:01 +0300

    defined todo item data model.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+}
